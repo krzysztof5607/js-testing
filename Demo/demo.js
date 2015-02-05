61ec39bf9cc3b90b24021497f9d6cb6b1556d905
Scott Chacon <schacon@gmail.com>
2015-02-05 20:32:00 +0100

    my commit message

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,11 +1,1 @@
-window.addEvent('domready', function() {
-	new Request.HTML({
-		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
-		data: {'delay': 2},
-		method: 'post',
-		update: 'demo',
-		onSuccess: function(response) {
-			$('demo').highlight();
-		}
-	}).send();
-})
+my updated file contents
