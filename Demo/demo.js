7fe0c9a74c84d7ad06313eefcafbb9a1d20683bd
krzysztof5607 <krzysztof5607@gmail.com>
2015-02-05 19:27:45 +0100

    Update demo.js

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,7 +1,7 @@
 window.addEvent('domready', function() {
 	new Request.HTML({
 		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
-		data: {'delay': 1},
+		data: {'delay': 2},
 		method: 'post',
 		update: 'demo',
 		onSuccess: function(response) {
